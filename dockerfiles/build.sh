01f0db6fbd39acefc7862f27c9981f1a1e08bb67
Mike Lambert <lambert8@illinois.edu>
2016-03-26 00:57:15 +0000

    Updated existing extractors and added new ones from extractors-core

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -4,7 +4,8 @@
 #
 # Latest stable tag: 0.9.2
 
-IMAGES=(clowder image-preview video-preview plantcv toolserver)
+IMAGES=(clowder toolserver)
+EXTRACTORS=(image-preview video-preview plantcv image-metadata audio-preview audio-speech2text pdf-preview)
 
 TAG=$1
 if [[ "$1" == "" ]]; then
@@ -16,11 +17,19 @@ for i in "${IMAGES[@]}"; do
 	docker build -t ndslabs/$i:${TAG} $i
 done
 
+for i in "${EXTRACTORS[@]}"; do
+	docker build -t ndslabs/$i:${TAG} extractors/$i
+done
+
 # Temporary hack to get this image working
 docker tag ndslabs/toolserver:${TAG} ncsa/clowder-toolserver:latest
 
-if [[ "${@/-p/}" == "$@" ]]; then
+if [[ "${@/-p/}" != "$@" ]]; then
 	for i in "${IMAGES[@]}"; do
+		docker push ndslabs/$i:${TAG}
+	done
+
+	for i in "${EXTRACTORS[@]}"; do
 		docker push ndslabs/$i:${TAG}
 	done
 fi
